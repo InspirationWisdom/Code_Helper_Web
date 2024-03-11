00e6d1fa5d6d15ee5aac3fc73c1a03f3e261b3d8
Zheng Lian <ss4g0923@gmail.com>
2024-03-11 12:32:54 +0800

    Update: building templates

---
+++ src/main/java/com/szcu/CodeHelperWebApplication.java
@@ -0,0 +1,13 @@
+package com.szcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHelperWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeHelperWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szcu/CodeHelperWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeHelperWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
