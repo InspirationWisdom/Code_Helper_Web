c8c3967bf9b151035ad85bbe20a602483a29e4a6
Zheng Lian <ss4g0923@gmail.com>
2024-03-23 18:10:45 +0800

    Feature: added new page for python code execution

---
+++ src/Frontend/src/pythonPage.vue
@@ -0,0 +1,106 @@
+<script setup>
+    import leftbar from './components/sidebar.vue'
+    import axios from 'axios'
+
+    const run = async () => {
+        let pythonCode = document.getElementById('python-code').value;
+        let outPut = document.getElementById('output');
+
+        await axios.post('http://localhost:8080/executePython', {
+            code: pythonCode
+        }).then((response) => {
+            outPut.innerHTML = response.data;
+        }).catch((error) => {
+            outPut.contentDocument.body.innerHTML = error;
+        });
+
+    }
+</script>
+
+<template>
+    <div class="container">
+        <leftbar/>
+        <div class="left">
+            <label for="python-code">Python</label>
+            <textarea id="python-code"></textarea>
+            <div class="button">
+                <button v-on:click="check()">Check Code</button>
+                <button v-on:click="run()">Run Code</button>
+            </div>
+        </div>
+        <div class="right">
+            <label for="output">Output</label>
+            <textarea id="output" srcdoc="" readonly></textarea>
+        </div>
+    </div>
+</template>
+
+<style>
+    @import './assets/base.css';
+
+    .container{
+        background: #454545;
+        color: #fff;
+        font-family: 'Poppins', sans-serif;
+        width: 100%;
+        height: 100vh;
+        display: flex;
+    }
+
+    .left, .right{
+        flex-basis: 50%;
+        padding: 10px;
+    }
+
+    textarea{
+        width: 100%;
+        height: 88%;
+        background: #1f1f1f;
+        color: #fff;
+        font-family: Consolas, monospace;
+        padding: 10px 20px;
+        border: 0;
+        outline: 0;
+        font-size: 18px;
+        overflow: scroll;
+    }
+
+    iframe{
+        width: 100%;
+        height: 95%;
+        background: #fff;
+        border: 0;
+        outline: 0;
+    }
+
+    label{
+        display: flex;
+        align-items: center;
+        background: #000;
+        height: 30px;
+        padding-left: 10px;
+    }
+
+    .button {
+        display: flex;
+        flex-direction: row;
+        float: right;
+    }
+
+    button {
+        display: block;
+        width: 90px;
+        line-height: 40px;
+        padding: 0px 5px 0px 5px;
+        margin-left: 40px;
+        margin-top: 10px;
+        border-radius: 15px;
+        border: black 2px solid;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background: rgb(202, 192, 192);
+        transition: all 0.3s;
+    }
+</style>
