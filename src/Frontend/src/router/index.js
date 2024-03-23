17a6341af3d571827ed15d0388918ad91319af6d
Zheng Lian <ss4g0923@gmail.com>
2024-03-23 18:09:17 +0800

    Update: added router for python code page

--- src/Frontend/src/router/index.js
+++ src/Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import h5c3 from '../h5c3.vue'
 import welcome from '../welcome.vue'
+import python from '../pythonPage.vue'
 
 
 const router = createRouter({
@@ -25,6 +26,14 @@ const router = createRouter({
             meta: {
               title: "HTML + CSS3 Page",
             }
+        },
+        {
+            path: "/python",
+            name: "Python",
+            component: python,
+            meta: {
+              title: "Python Page",
+            } 
         },
     ]
 })
