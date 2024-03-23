2b2e7d94a166bb869553b1dd952d7c73df89587a
Zheng Lian <ss4g0923@gmail.com>
2024-03-23 15:04:57 +0800

    Update: added welcome page

---
+++ src/Frontend/src/welcome.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import leftbar from './components/sidebar.vue'
+</script>
+
+<template>
+    <div class="container">
+        <leftbar/>
+        <div class="page">
+            <div class="text">
+                Welcome to the Code Helper
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+  @import './assets/base.css';
+
+  .container{
+    background: #454545;
+    color: #fff;
+    font-family: 'Poppins', sans-serif;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+  }
+
+  .page{
+    height: 100vh;
+    width: 100%;
+    display: flex;
+  }
+
+  .text{
+    margin: 0 auto;
+    font-size: 40px;
+    margin-top: 45vh;
+  }
+</style>
