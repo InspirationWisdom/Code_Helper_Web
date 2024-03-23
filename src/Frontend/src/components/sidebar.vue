1c7b13998b7f1d5b7c87fbc1936c1c6ab6179f72
Zheng Lian <ss4g0923@gmail.com>
2024-03-23 15:03:26 +0800

    Update: added side bar component for the web page

---
+++ src/Frontend/src/components/sidebar.vue
@@ -0,0 +1,50 @@
+<script setup></script>
+
+<template>
+     <div class="leftbar">
+            <div class="list">
+                <ul>
+                    <li><a href="/home">About</a></li>
+                    <li><a href="/python">Python</a></li>
+                    <li><a href="/html">Web Frontend</a></li>
+                </ul>
+            </div>
+        </div>
+</template>
+
+<style scope>
+    *{
+        margin: 0;
+        padding: 0;
+        font-family: 'Poppins', sans-serif;
+        box-sizing: border-box;
+    }
+
+    .leftbar{
+        width: 10%;
+        height: 100vh;
+        border-right: #333 2px solid;
+        background: #454545;
+        margin: 10px;
+    }
+
+    .list ul{
+        list-style: none;
+    }
+
+    .list ul li{
+        padding: 30px 0px 30px 10px;
+        color: #fff;
+        cursor: pointer;
+        border-bottom: #333 2px solid;
+    }
+
+     .list ul li a{
+        text-decoration: none;
+        color: #fff;
+    }
+
+    .list ul li:hover{
+        background: #333;
+ }
+</style>
