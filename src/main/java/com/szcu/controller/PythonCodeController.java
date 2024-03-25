5fee32866258ebf937fda973b2747aa97594e88a
Zheng Lian <ss4g0923@gmail.com>
2024-03-25 23:00:32 +0800

    Feature: modified code to support pass arguments into python code

--- src/main/java/com/szcu/controller/PythonCodeController.java
+++ src/main/java/com/szcu/controller/PythonCodeController.java
@@ -4,7 +4,12 @@ import com.szcu.model.CodeWrapper;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -14,32 +19,41 @@ public class PythonCodeController {
 	@ResponseBody
 	public String executePythonCode(@RequestBody CodeWrapper codeWrapper) {
 		String code = codeWrapper.getCode();
-		ProcessBuilder processBuilder = new ProcessBuilder();
-		// Assuming Python is installed and in the system's PATH
-		processBuilder.command("python3", "-c", code);
+		String params = codeWrapper.getParams();
+		List<String> command = new ArrayList<>(Arrays.asList("python3", "-c", code));
+
+		ProcessBuilder processBuilder = new ProcessBuilder(command);
 
 		try {
 			Process process = processBuilder.start();
+
+			try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()))) {
+				// Write the input to the process's input stream
+				writer.write(params);
+				writer.newLine();
+				writer.flush();
+			}
+
 			StringBuilder output = new StringBuilder();
 			StringBuilder errors = new StringBuilder();
 
-			BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+			try (BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				 BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
 
-			String line;
-			while ((line = stdInput.readLine()) != null) {
-				output.append(line).append("\n");
-			}
+				String line;
+				while ((line = stdInput.readLine()) != null) {
+					output.append(line).append("\n");
+				}
 
-			while ((line = stdError.readLine()) != null) {
-				errors.append(line).append("\n");
+				while ((line = stdError.readLine()) != null) {
+					errors.append(line).append("\n");
+				}
 			}
 
 			int exitVal = process.waitFor();
 			if (exitVal == 0) {
 				return output.toString();
 			} else {
-				// Return error information if the script execution fails
 				return "Error in executing the script:\n\n" + errors.toString();
 			}
 		} catch (Exception e) {
