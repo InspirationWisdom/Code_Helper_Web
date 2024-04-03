cf24c4d75f4f127efb9f2af34f028bd819c95c2f
Zheng Lian <ss4g0923@gmail.com>
2024-04-03 17:39:04 +0800

    Feature: added new banner component

--- src/Frontend/src/components/banner.vue
+++ src/Frontend/src/components/banner.vue
@@ -3,11 +3,23 @@
 </script>
 
 <template>
-    <div class="container">
-        
+    <div class="wrapper">
+        <p>Code Helper</p>
     </div>
 </template>
 
 <style scoped>
+    .wrapper{
+        height: 13vh;
+        background-color: #f5f5f5;
+        border-bottom: 2px solid #e0e0e0;
+    }
 
+    .wrapper p{
+        font-size: 35px;
+        color: black;
+        font-family: Consolas, monospace;
+        line-height: 13vh;
+        margin-left: 40%;
+    }
 </style>
